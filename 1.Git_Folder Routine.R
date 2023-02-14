cb2cbcd8287ad10207b162c3b872087b6d247d28
Sam Deegan <sam.deegan@ucdconnect.ie>
2023-02-14 13:12:12 +0000

    Initial File Structure Set-Up

--- 1.Git_Folder Routine.R
+++ 1.Git_Folder Routine.R
@@ -1,23 +1,31 @@
 
+### Set Working Directory #####################################################
+
+  # Choose Working folder
+setwd(choose.dir())
+
 ### Code Generating Root Folders ##############################################
 
-#Define Top-Level Directories
+  # Define Top-Level Directories
 directories <- c("build", "analysis")
 
-# Define the subdirectories for each top-level directory
+  # Define the subdirectories for each top-level directory
 subdirectories <- c("input", "code", "output", "temp")
 
-# Create the directory structure
+  # Create the directory structure
 for (dir in directories) {
-  # Create the top-level directory
+    # Create the top-level directory
   dir.create(dir)
   
-  # Create the subdirectories within the top-level directory
+    # Create the subdirectories within the top-level directory
   for (subdir in subdirectories) {
     dir.create(file.path(dir, subdir))
   }
 }
 
 ###############################################################################
-
-
+library("usethis")
+usethiss:: git remote set-url origin https://github.com/yourusername/rstudio-git-test2.git
+usethiss:: git remote add upstream https://github.com/yourusername/rstudio-git-test2.git
+usethiss:: git push origin master
+usethiss:: git push --all
